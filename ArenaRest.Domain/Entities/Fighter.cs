ae922879ba2b4ed889f1b79d40343179cbda2aa9
Crunza17 <crisyolo.cr17@gmail.com>
2026-01-09 17:20:01 +0100

    Fighter created and tested

---
+++ ArenaRest.Domain/Entities/Fighter.cs
@@ -0,0 +1,29 @@
+﻿namespace ArenaRest.Domain.Entities;
+
+public class Fighter
+{
+    public string Name { get; private set; }
+    public int MaxHp { get; private set; }
+    public int CurrentHp { get; private set; }
+    public int Strength { get; private set; }
+    
+    public bool IsDead => CurrentHp == 0;
+
+    public Fighter(string name, int maxhp, int strength)
+    {
+        Name = name;
+        MaxHp = maxhp;
+        Strength = strength;
+        CurrentHp = maxhp;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        CurrentHp = Math.Max(0, CurrentHp - amount);
+    }
+    
+    public void Attack(Fighter target)
+    {
+        target.TakeDamage(this.Strength);
+    }
+}
---
+++ ArenaRest.Test/FighterTests.cs
@@ -0,0 +1,48 @@
+﻿using ArenaRest.Domain.Entities;
+using Shouldly;
+
+namespace ArenaRest.Test;
+
+public class FighterTests
+{
+    [Fact]
+    public void Fighter_Should_Reduce_Hp_When_Taking_Damage()
+    {
+        var fighter = new Fighter("Hero", 100, 0); 
+
+        fighter.TakeDamage(10);
+
+        fighter.CurrentHp.ShouldBe(90);
+    }
+    
+    [Fact]
+    public void CurrentHp_Should_Not_Drop_Below_Zero()
+    {
+        var fighter = new Fighter("Weakling", 50, 0);
+
+        fighter.TakeDamage(100);
+
+        fighter.CurrentHp.ShouldBe(0);
+    }
+    
+    [Fact]
+    public void IsDead_Should_Be_True_When_Hp_Is_Zero()
+    {
+        var fighter = new Fighter("Zombie", 10, 0);
+
+        fighter.TakeDamage(10);
+
+        fighter.IsDead.ShouldBeTrue();
+    }
+    
+    [Fact]
+    public void Attack_Should_Reduce_Target_Hp_By_Attacker_Strength()
+    {
+        var attacker = new Fighter("Hero", 100, 10); 
+        var target = new Fighter("Zombie", 50, 5);
+
+        attacker.Attack(target);
+
+        target.CurrentHp.ShouldBe(40);
+    }
+}
